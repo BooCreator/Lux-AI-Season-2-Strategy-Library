3e34924bcacff2fd2101dd992cedd5a731019fd4
BooCreator <boocreator.official@gmail.com>
2023-03-01 16:14:53 +0100

    Update competition.py
    Добавлен вывод для видео _e_{количество шагов, прошедших в игре}

--- utils/competition.py
+++ utils/competition.py
@@ -134,7 +134,7 @@ class LuxAI:
         for folder in ['log', 'video', datefolder]:
             full_path += folder + '/'
             if not os.path.exists(full_path): os.mkdir(full_path)
-        full_path += datename + f'_s_{steps}'
+        full_path += datename + f'_s_{steps}_e_{step}'
         return toVideo(imgs, full_path)
 # ===============================================================================================================
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
