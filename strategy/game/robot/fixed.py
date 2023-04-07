6164e5dd1f1fcdbc350fd9fde7a150b5606cce71
BooCreator <boocreator.official@gmail.com>
2023-04-07 18:09:48 +0300

    Update fixed.py

--- strategy/game/robot/fixed.py
+++ strategy/game/robot/fixed.py
@@ -181,5 +181,10 @@ class RobotStrategy:
             # --- выясняем куда мы можем шагнуть ---
             if not actions.buildMove(item.getNeareastPoint(unit.pos), True, 1, lock_map):
                 actions.buildMove(item.getNeareastPoint(unit.pos), True, 1)
-        
+        # --- если робот не на фабрике и он - уничтожитель ---
+        elif task == ROBOT_TASK.DESTROYER:
+            pass
+            # --- выясняем куда мы можем шагнуть ---
+            #if not actions.buildMove(item.getNeareastPoint(unit.pos), True, 1, lock_map):
+            #    actions.buildMove(item.getNeareastPoint(unit.pos), True, 1)
         return actions
